122ef9e5d078108348465d934cd27f0fc052d81f
Subash Bhattachan <bash_shoe@hotmail.com>
2024-12-07 17:52:31 -0500

    form with states and props concepts

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,109 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
-function Header() {
-  return (<h1>Simple React Application</h1>);
+// function Header() {
+//   return (<h1>Simple React Application</h1>);
+// }
+
+// function Content(props) {
+//   return (<p style={{ color: props.color }}>{props.text}</p>);
+// }
+
+// function Footer() {
+//   return (<h1>Created by Me, of course.</h1>);
+// }
+
+// Header component
+function Header ({ message }) {
+  return <h1> {message}</h1>;
 }
 
-function Content(props) {
-  return (<p style={{color: props.color}}>{props.text}</p>);
-}
+// FormInput component
+function FormInput ({ name, value, placeholder, onChange }) {
+  return {
 
-function Footer() {
-  return (<h1>Created by Me, of course.</h1>);
+    
+  }
+
+
 }
 
 
 function App() {
-  
+
+  const [fullName, setFullName] = useState({
+    fname: "",
+    lname: "",
+  });
+
+  const [message, setMessage] = useState("");
+
+  const inputEvent = (event) => {
+    console.log(event.target.value);
+    console.log(event.target.name);
+
+    const value = event.target.value;
+    const name = event.target.name;
+
+    setFullName((preValue) => {
+      //console.log(preValue)
+      if (name === "fName") {
+        return {
+          fname: value,
+          lname: preValue.lname,
+        }
+      } else if (name === "lName") {
+        return {
+          fname: preValue.fname,
+          lname: value,
+        }
+
+      }
+
+    }
+    )}
+
+  const onSubmits = (event) => {
+    event.preventDefault();
+    //alert("Form submitted")
+    // Show success message
+    setMessage(`Form submitted by ${fullName.fname} ${fullName.lname}`);
+    setFullName ({
+      fname: "",
+      lname: "",
+    })
+  };
 
   return (
     <>
-    <Header />
-    <Content color="blue" text="This is my first React Application!" />
-    <Content color="red" text="Wish me luck..." />
-    <Content color="green" text="I think I have got it!" />
-    <Footer />
-    
+      <div className="main-div">
+
+        <Header message={message} />
+        
+        <form onSubmit={onSubmits}>
+          <div>
+            <h1>Hello</h1>
+            <input
+              type="text"
+              placeholder="Enter Your Name"
+              name="fName"
+              onChange={inputEvent}
+            value={fullName.fname}
+            />
+          <br />
+            <input
+              type="text"
+              placeholder="Enter Your Last Name"
+              name="lName"
+              onChange={inputEvent}
+            value={fullName.lname}
+            />
+            <button type="submit"> Submit ✔ </button>
+          </div>
+        </form>
+      </div>
     </>
   )
 }
